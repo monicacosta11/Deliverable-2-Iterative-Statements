0d7975a990d451130e043ac640100726f20fb8cc
Monica Costa <monicacosta@usf.edu>
2019-01-24 17:38:17 -0500

    update

--- Deliverable 2 Iterative Statements/Program.cs
+++ Deliverable 2 Iterative Statements/Program.cs
@@ -26,24 +26,29 @@ namespace Deliverable_2_Iterative_Statements
                 //This variable will be used to perform the various iterative statements and is parsed as int
                 int value_of_input = int.Parse(input);
 
-               
-                // Here is the For Loop
-                for (int i = 0; i < value_of_input; i++)
+                if (value_of_input > 100)
                 {
-                    Console.WriteLine("You have entered " + i.ToString() + " This is the current integer value in the loop:");
+                    Console.WriteLine("You've entered a grade above 100 and Dr. Daniel does not give extra credit. Try again!");
+                    Console.ReadKey(true);
                 }
+                else
+                {
+                    // Here is the For Loop
+                    for (int i = 0; i < value_of_input; i++)
+                    {
+                        Console.WriteLine("You have entered " + value_of_input + " This is the current integer value in the loop:" + i.ToString());
+                    }
 
-                Console.WriteLine("Press any key to exit");
-                // Pause the program and wait for the user to press a key and end the program
-                Console.ReadKey(true);
-
-            } //End of try
-
+                    Console.WriteLine("Press any key to exit");
+                    // Pause the program and wait for the user to press a key and end the program
+                    Console.ReadKey(true);
+                } //End of try
+            }
             catch
             {
-                Console.WriteLine("There was an error");
+                Console.WriteLine("There was an error :/");
                 Console.ReadKey(true);
             } //End of catch
-        }
-    }
-}
+        } // End of main
+    } // End of class
+} // End of mainspace
